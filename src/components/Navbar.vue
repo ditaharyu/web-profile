1ef0a84e7d92b3563708adf1ccf0bc80186b0fb3
Anindhita Haryuningtyas <anindhitaharyu@gmail.com>
2020-04-26 01:38:20 +0700

    add: new project for personal web-profile

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div id="nav">
+      <router-link to="/">
+        <b-button variant="white" class="bg-primary text-white">Home</b-button>
+      </router-link>
+      <router-link to="/about">
+        <b-button variant="white" class="bg-warning">About</b-button>
+      </router-link>
+      <router-link to="/contact">
+        <b-button variant="white" class="bg-danger text-white">Contact</b-button>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+
+</style>
---
+++ src/views/Contact.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <h1>This is contact</h1>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
